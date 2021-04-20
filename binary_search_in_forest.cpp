13d273a56dd39922b4521c55439e571e8a83f270
adityarana14 <49364001+adityarana14@users.noreply.github.com>
2021-04-21 05:08:27 +0530

    Update binary_search_in_forest.cpp

--- binary_search_in_forest.cpp
+++ binary_search_in_forest.cpp
@@ -3,38 +3,48 @@ class Solution{
     int find_height(int tree[], int n, int k)
     {
         // code here
-        sort(tree, tree+n);
-        int i=0;
-        int j=tree[n-1];
-        
-        
-        while(i<=j)
+        priority_queue<int> pq;
+        for(int i=0;i<n; i++)
         {
-            int mid = (i+j)/2;
-            
-            int val =0;
-            for(int k=0;k<n;k++)
-            {
-                if(tree[k]>mid)
-               val = val + tree[k]-mid;
-            }
-             
-            
-            if(val == k )
-            {
-                return mid;
-            }
-            if(val > k)
-            {
-                i=mid+1;
-            }
-            
-            if(val < k )
-            {
-                j=mid-1;
-            }
+            pq.push(tree[i]);
         }
         
-        return -1 ;
+        int min = 0;
+        int max = pq.top();
+        
+        
+        while(min<=max)
+        {
+            int middle= (min + max )/2;   // middle number as our first point of reference 
+            
+            int value_got= 0;
+            for(int i=0;i<n;i++)
+            {
+            
+                if(tree[i]>middle)
+                {
+                    value_got+=tree[i]-middle;
+                }
+                
+            }
+            
+            if(value_got==k)
+            return middle;
+            
+            if(value_got > k )
+            {
+                min = middle + 1;
+            }
+            
+            
+            if(value_got <k)
+            {
+                max=middle-1;
+            }
+            
+            
+        }
+        
+        return -1;
     }
 };
